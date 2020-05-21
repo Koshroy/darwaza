5c21fc85a7254cc92ea05aff35e29a8690819dcd
Koushik Roy <koushik@meff.me>
2020-05-21 11:58:43 -0700

    Basic Gemini client being wired into the browser

--- gemini.tcl
+++ gemini.tcl
@@ -1,11 +1,29 @@
 package require uri
 package require tls
 
+namespace eval ::gemini {
+    namespace export fetch
+}
+
 uri::register gemini {
     variable schemepart $uri::http::schemepart
 }
 
+# uri::Split<Scheme> is the proc called when trying to
+# call uri::split on a uri.
+# This will be called when we call uri::fetch [gemini://cool.website]
 proc uri::SplitGemini {url} {
+    # The easiest way to hook into the uri package and add our own
+    # gemini scheme is to simply piggyback off what html already offers
+    # us.
+    # So:
+    # 1. Take the url and change the scheme to point to http
+    # 2. Call uri::SplitHttpInner, the helper function that
+    #    both uri::SplitHttp and uri::SplitHttps use under
+    #    the hood
+    # 3. Set some sane defaults, i.e. if the port is not provided
+    #    initialize it to 1965
+    # 4. Return the split array
     set sub_url [string map {gemini:// http://} $url]
     array set split_url [uri::SplitHttpInner gemini $sub_url]
     if {$split_url(port) eq ""} {
@@ -19,13 +37,39 @@ proc uri::SplitGemini {url} {
     return [array get split_url]
 }
 
-proc fetch {url} {
+proc mime_type {header} {
+    set code_len 2
+    set mime_start_ind [expr $code_len + 1]
+    set mime_end_ind [expr [string first ";" $header $mime_start_ind] - 1]
+    ;# If we know that the mime type has to at least start at the end of
+    ;# the status code, and that there is no way that we found the
+    ;# mimetype, so we are in error.
+    ;# This is a more restrictive check than $mime_end_ind < 0
+    if { $mime_end_ind < $code_len} {
+        return ""
+    }
+    return [string range $header $mime_start_ind $mime_end_ind]
+}
+
+proc ::gemini::fetch {url} {
     array set url_split [uri::split $url]
     set sock [tls::socket $url_split(host) $url_split(port)]
     chan puts -nonewline $sock [string cat $url "\r\n"]
     chan flush $sock
-    set resp [chan read $sock]
-    chan close $sock
-    return $resp
-}
 
+    gets $sock header ;# Grab the header line from the socket
+    set code [string range $header 0 1]
+    if {[string index $code 0] eq 5} {
+        error "Error fetching Resource: $header"
+    }
+
+    set body [chan read $sock]
+    close $sock
+
+
+    set resp(code) $code
+    set resp(mime-type) [mime_type $header]
+    set resp(body) $body
+    
+    return [array get resp]
+}
---
+++ main.tcl
@@ -0,0 +1,51 @@
+package require Tk
+
+source "gemini.tcl"
+
+# History list
+set loc_hist [list]
+
+set browser_title "Darwaza"
+set start_page "gemini://acidic.website"
+set browser_url $start_page
+set viewport_contents ""
+
+wm title . "Darwaza"
+
+pack [ttk::frame .r1] ;# Address Bar
+pack [ttk::frame .r2] ;# Gemini Viewport
+pack [ttk::frame .r3] ;# Status Bar
+
+
+set back_btn [ttk::button .r1.back -text ◀ -width 3]
+set fwd_btn [ttk::button .r1.forward -text ▶ -width 3]
+set address_bar [ttk::entry .r1.address -textvariable browser_url]
+set go_btn [ttk::button .r1.go -text "Go!" -command change_url]
+
+set regular_font [font create "view_regular" -family "Georgia" -size 13]
+set viewport [text .r2.viewport]
+set statusbar [ttk::label .r3.statusbar -justify right -text "Welcome to Darwaza!"]
+
+
+$viewport configure -font $regular_font
+
+proc change_url {} {
+    array set resp [gemini::fetch $::browser_url]
+    $::viewport delete 1.0 [$::viewport index end]
+    $::viewport insert 1.0 $resp(body)
+}
+
+
+# pack configure .r1 -fill x; pack configure .r2 -fill both -expand 1
+pack $back_btn -side left -padx 2
+pack $fwd_btn -side left -padx 2
+pack $address_bar -side left -fill both -expand true -padx 3 -pady 3
+pack $go_btn -side left -fill both -expand true -padx 3 -pady 3
+pack $statusbar -fill x
+pack $viewport -side left -fill both -expand 1
+
+focus $address_bar
+
+foreach w [winfo children .r1] {pack configure $w -fill x}
+foreach w [winfo children .r2] {pack configure $w -fill both -expand 1}
+foreach w [winfo children .r3] {pack configure $w -fill x -expand 1}
